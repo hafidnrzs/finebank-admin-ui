17145de1c2ed7ed0b58d3e9deed3f1e286b3ebf0
almiraazs <aalmirafiraa@gmail.com>
2024-11-23 01:28:56 +0700

    Icon Transaction

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,55 +1,141 @@
 import Card from "../components/Elements/Card";
 import MainLayout from "../components/Layout/MainLayout";
 import bills from "../data/bills";
-//import expensesBreakdown from "../data/expenses";
-//import transactions from "../data/transactions";
+import expensesBreakdown from "../data/expensesBreakdown";
+import transactions from "../data/transactions";
+import { useState } from "react";
 
 const Dashboard = () => {
+  const tabs = ["All", "Revenue", "Expense"];
+
+  const [trxs, setTrx] = useState(transactions);
+  const [activeTab, setActiveTab] = useState("All");
+
+  function handleClick(e) {
+    const transactionsFiltered =
+      e.target.value != "All"
+        ? transactions.filter(({ type }) => type == e.target.value)
+        : transactions;
+
+    setActiveTab(e.target.value);
+    setTrx(transactionsFiltered);
+  }
+
   const billCard = bills.map((bill) => (
-    <div key={bill.id} className="flex">
+    <div key={bill.id} className="lg:flex justify-between pt-3 pb-3">
       <div className="flex">
-        <div>
-          {bill.month}
-          <br />
-          {bill.date} 
+        <div className="bg-special-bg me-3 px-4 rounded-lg flex place-content-center flex-col">
+          <span className="text-xs">{bill.month}</span>
+          <span className="text-2xl font-bold">{bill.date}</span>
         </div>
         <div>
-          <img src={`/images/${bill.logo}`} /> 
-          {bill.name}
+          <img className="h-6" src={`/images/${bill.logo}`} />
+          <span className="font-bold">{bill.name}</span>
           <br />
-          Last Charge - {bill.lastCharge}
+          <span className="text-xs">Last Charge - {bill.lastCharge}</span>
         </div>
       </div>
-      <div>${bill.amount}</div>
+      <div className="flex place-content-center flex-col">
+        <span className="p-2 border rounded-lg font-bold text-center">
+          ${bill.amount}
+        </span>
+      </div>
     </div>
   ));
-  //const expenseCard = expensesBreakdown.map((expense) => <div key={expense.id}>{expense.name}</div>);
-  //const transactionCard = transactions.map((transaction) => <div key={transaction.id}>{transaction.name}</div>);
+  const expenseCard = expensesBreakdown.map((expense) => (
+    <div key={expense.id} className="flex pb-4 justify-between">
+      <div className="flex">
+        <div className="bg-special-bg px-3 rounded-lg flex flex-col place-content-center">
+          {/* {expense.icon} */}
+        </div>
+        <div className="ms-4">
+          <span className="text-gray-02">{expense.category}</span>
+          <br />
+          <span className="font-bold text-lg">${expense.amount}</span>
+          <div className="flex">
+            <span className="text-gray-02">{expense.percentage}%</span>{" "}
+            {/* {expense.arrow} */}
+          </div>
+        </div>
+      </div>
+      <div className="flex place-content-center flex-col me-8">
+        {/* <Icon.ArrowRight /> */}
+      </div>
+    </div>
+  ));
+  const transactionCard = trxs.map((transaction) => (
+    <div key={transaction.id} className="flex justify-between my-6">
+      <div className="flex">
+        <div className="bg-special-bg px-3 rounded-lg flex flex-col place-content-center">
+          {transaction.icon}
+        </div>
+        <div className="ms-4">
+          <span className="text-xl font-bold">
+            {transaction.transactionName}
+          </span>
+          <br />
+          <span className="text-gray-02">{transaction.shopName}</span>
+        </div>
+      </div>
+      <div className="text-right">
+        <span className="text-xl font-bold text-gray-02">
+          ${transaction.amount}
+        </span>
+        <br />
+        <span className="text-gray-02">{transaction.date}</span>
+      </div>
+    </div>
+  ));
   return (
     <MainLayout type="dashboard">
       {/* top content start*/}
-        <div className="md:grid md:grid-cols-3 md:gap-x-6">
-            <Card title="Total Balance" />
-            <Card 
-                title="Goals" 
-                desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, omnis optio. Adipisci, eum ipsa eaque consequuntur dolorum recusandae, officia explicabo quos, sequi sunt corporis eos minima modi nam et id?"
-            />
-            <Card title="Upcoming Bills" desc={billCard} />
+      <div className="md:grid md:grid-cols-3 md:gap-x-6">
+        <Card title="Total Balance" />
+        <Card
+          title="Goals"
+          desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, omnis optio. Adipisci, eum ipsa eaque consequuntur dolorum recusandae, officia explicabo quos, sequi sunt corporis eos minima modi nam et id?"
+        />
+        <Card title="Upcoming Bills" desc={billCard} />
+      </div>
+      {/* top content end*/}
+      {/* bottom content start*/}
+      <div className="md:grid md:grid-cols-3 md:gap-x-6">
+        <div className="md:col-span-1">
+          <Card
+            variant="md:col-span-1 md:row-span-2"
+            title="Recent Transaction"
+            desc={
+              <div>
+                <div className="mb-4">
+                  {tabs.map((tab) => (
+                    <button
+                      className={
+                        activeTab == tab
+                          ? "px-4 font-bold border-b-4 border-primary text-primary"
+                          : "px-4 font-bold text-gray-01"
+                      }
+                      key={tab}
+                      value={tab}
+                      onClick={handleClick}
+                    >
+                      {tab}
+                    </button>
+                  ))}
+                </div>
+                {transactionCard}
+              </div>
+            }
+          />
         </div>
-        {/* top content end*/}
-        {/* bottom content start*/} 
-        <div className="md:grid md:grid-cols-3 md:gap-x-6">
-            <div className="md:col-span-1">
-                <Card 
-                    title="Recent Transaction" 
-                    desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, omnis optio. Adipisci, eum ipsa eaque consequuntur dolorum recusandae, officia explicabo quos, sequi sunt corporis eos minima modi nam et id?"
-                />
-            </div>
-            <div className="md:col-span-2 flex flex-col flex-1">
-                <Card title="Statistics" />
-                <Card title="Expenses Breakdown" />
-            </div>
+        <div className="md:col-span-2 flex flex-col flex-1">
+          <Card variant="md:col-span-2" title="Statistics" />
+          <Card
+            variant="md:col-span-2"
+            title="Expenses Breakdown"
+            desc={<div className="lg:grid lg:grid-cols-3">{expenseCard}</div>}
+          />
         </div>
+      </div>
       {/* bottom content end*/}
     </MainLayout>
   );
