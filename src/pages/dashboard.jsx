7e3418d6195fd0c9f05496f8ae4c84c1a0c9ab72
hafidnrzs <hafidnurazis@gmail.com>
2024-11-22 23:54:05 +0700

    feat: add Expenses Breakdown

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,8 +1,9 @@
+// import { ArrowRight } from "@mui/material";
 import Card from "../components/Elements/Card";
 import MainLayout from "../components/Layout/MainLayout";
 import bills from "../data/bills";
-// import expensesBreakdown from "../data/expensesBreakdown";
-// import transactions from "../data/transactions";
+import expensesBreakdown from "../data/expensesBreakdown";
+import transactions from "../data/transactions";
 
 const Dashboard = () => {
   const billCard = bills.map((bill) => (
@@ -26,8 +27,30 @@ const Dashboard = () => {
       </div>
     </div>
   ));
-  //const expenseCard = expensesBreakdown.map((expense) => <div key={expense.id}>{expense.name}</div>);
-  //const transactionCard = transactions.map((transaction) => <div key={transaction.id}>{transaction.name}</div>);
+  const expenseCard = expensesBreakdown.map((expense) => (
+    <div key={expense.id} className="flex pb-4 justify-between">
+      <div className="flex">
+        <div className="bg-special-bg px-3 rounded-lg flex flex-col place-content-center">
+          {/* {expense.icon} */}
+        </div>
+        <div className="ms-4">
+          <span className="text-gray-02">{expense.category}</span>
+          <br />
+          <span className="font-bold text-lg">${expense.amount}</span>
+          <div className="flex">
+            <span className="text-gray-02">{expense.percentage}%</span>{" "}
+            {/* {expense.arrow} */}
+          </div>
+        </div>
+      </div>
+      <div className="flex place-content-center flex-col me-8">
+        {/* <Icon.ArrowRight /> */}
+      </div>
+    </div>
+  ));
+  const transactionCard = transactions.map((transaction) => (
+    <div key={transaction.id}>{transaction.name}</div>
+  ));
   return (
     <MainLayout type="dashboard">
       {/* top content start*/}
@@ -44,13 +67,18 @@ const Dashboard = () => {
       <div className="md:grid md:grid-cols-3 md:gap-x-6">
         <div className="md:col-span-1">
           <Card
+            variant="md:col-span-1 md:row-span-2"
             title="Recent Transaction"
-            desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, omnis optio. Adipisci, eum ipsa eaque consequuntur dolorum recusandae, officia explicabo quos, sequi sunt corporis eos minima modi nam et id?"
+            desc={transactionCard}
           />
         </div>
         <div className="md:col-span-2 flex flex-col flex-1">
-          <Card title="Statistics" />
-          <Card title="Expenses Breakdown" />
+          <Card variant="md:col-span-2" title="Statistics" />
+          <Card
+            variant="md:col-span-2"
+            title="Expenses Breakdown"
+            desc={<div className="lg:grid lg:grid-cols-3">{expenseCard}</div>}
+          />
         </div>
       </div>
       {/* bottom content end*/}
