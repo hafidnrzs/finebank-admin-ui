baa252c75a77e9450bcc5bd9b9b7512f6ce5027b
almiraazs <aalmirafiraa@gmail.com>
2024-12-05 23:05:30 +0700

    card statistic

--- src/components/Fragments/dashboard/CardStatistic.jsx
+++ src/components/Fragments/dashboard/CardStatistic.jsx
@@ -1,4 +1,4 @@
-import { expensesStatistics } from "../../../data/expensesBr";
+import { expensesStatistics } from "../../../data/expensesBreakdown";
 import BarsDataset from "../../Elements/BarChart/Index";
 import Card from "../../Elements/Card";
 
@@ -6,7 +6,11 @@ const CardStatistic = () => {
     const expensesStatisticCard = expensesStatistics.map((expensesStatistic) => (<div key={expensesStatistic.id}>{expensesStatistic.date}</div>));
 
     return (
-        <Card variant="lg:col-span-2 min-h-max" title="Statistics" desc=<BarsDataset desc={expensesStatisticCard} />
+        <Card 
+            variant="lg:col-span-2 min-h-max" 
+            title="Statistics" 
+            desc=<BarsDataset desc={expensesStatisticCard} /> 
+        />
     )
 }
 
